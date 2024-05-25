9cc01a97eb5b3ea22b1524c1afe9ea46b2f3eeb3
lrowe8 <ssbrllc@gmail.com>
2024-05-25 07:00:10 -0600

    Added sale line

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import yfinance as yf
 import dash
 from dash import dcc
 from dash import html
-import csv
+import pandas as pd
 import requests
 import json
 
@@ -11,42 +11,60 @@ headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
     }
 
-def update_graph(input_data:str):
+def update_graph(symbol:str, date_purchased:datetime, cost_per_share:float):
     # Set the timespan
-    # start = datetime.datetime(2010, 1, 1)
     end = datetime.datetime.now()
-    start = end - datetime.timedelta(days=(365 * 2))
+    start = end - datetime.timedelta(days=(200 * 2))
 
     try:
         # Get stock data from yahoo
-        df = yf.download(input_data, start, end)
+        df = yf.download(symbol, start, end)
 
         # Get the rolling averages
         df['20Day'] = df['Close'].rolling(20).mean()
         df['50Day'] = df['Close'].rolling(50).mean()
         df['200Day'] = df['Close'].rolling(200).mean()
 
+        # Adjust start date to purchase date if needed
+        if start < date_purchased:
+            start = date_purchased
+
+        # Get the maximum closing price of the stock after the start date
+        max_price = df.loc[start : datetime.datetime.now()]['Close'].max()
+
+        # If the maximum price is lower than the purchase price then we want a line at 7% below purchase
+        losing_sale_price = cost_per_share - (cost_per_share * .07)
+        winning_sale_price = max_price - (max_price * .03)
+
+        if winning_sale_price >= cost_per_share:
+            sale_price = max_price - (max_price * .03)
+        else:
+            sale_price = losing_sale_price
+        
+        df['SalePrice'] = sale_price
+
         # Remove data older than 90 days
         df = df[df.index >= (datetime.datetime.now() - datetime.timedelta(days=90))]
 
         # Create the graph
         graph = dcc.Graph(
-            id=f'graph-{input_data}', 
+            id=f'graph-{symbol}', 
             figure = 
             {
                 'data':[
-                    {'x':df.index, 'y':df.Close, 'type':'line', 'name':input_data},
+                    {'x':df.index, 'y':df.Close, 'type':'line', 'name':symbol},
                     {'x':df.index, 'y':df['20Day'], 'type':'line', 'name':'Short (20 Day)'},
                     {'x':df.index, 'y':df['50Day'], 'type':'line', 'name':'Medium (50 Day)'},
-                    {'x':df.index, 'y':df['200Day'], 'type':'line', 'name':'Long (200 Day)'}
+                    {'x':df.index, 'y':df['200Day'], 'type':'line', 'name':'Long (200 Day)'},
+                    {'x':df.index, 'y':df['SalePrice'], 'type':'line', 'name':'Sale Price'}
                     ],
                 'layout':{
-                    'title':input_data
+                    'title':symbol
                 },
             },
             style = 
             {
-                'border':'2px black solid',
+                'border':f'2px {"tomato" if df.iloc[-1]["Close"] < sale_price else "black"} solid',
             }
         )
     
@@ -99,12 +117,7 @@ def create_analyst(input_data:str):
 
 
 if __name__ == '__main__':
-    rows = []
-    with open('current_stocks.csv') as csvfile:
-        current_stocks = csv.DictReader(csvfile)
-        
-        for row in current_stocks:
-            rows.append(row)
+    stocks_xlsx = pd.read_excel('current-stocks.xlsx')
 
     app = dash.Dash()
     app.title = "Stock Visualization"
@@ -113,11 +126,11 @@ if __name__ == '__main__':
         html.H1("Stock Visualization Dashboard"),
     ])
 
-    for row in rows:
-        app.layout.children += [html.Div(id=f'{row["Symbol"]}-graph', style={'padding-bottom': '20px'}), 
-                                get_sentiment(row["Symbol"]),
-                                create_analyst(row["Symbol"]),
-                                update_graph(row["Symbol"]),
+    for _, record in stocks_xlsx.iterrows():
+        app.layout.children += [html.Div(id=f'{record["Symbol"]}-graph', style={'padding-bottom': '20px'}), 
+                                get_sentiment(record["Symbol"]),
+                                create_analyst(record["Symbol"]),
+                                update_graph(record["Symbol"], record["Date Purchased"], record["Cost Per Share"]),
                                 ]    
 
     app.run_server()
